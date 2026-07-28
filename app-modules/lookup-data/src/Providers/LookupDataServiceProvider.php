9f14f6f1431704ed2dc4103581c2f3d6a5ddab74
novanesthi <novanesthi@gmail.com>
2026-07-28 16:40:12 +0700

    feat(lookup): add bilingual lookup service and cache invalidation

--- app-modules/lookup-data/src/Providers/LookupDataServiceProvider.php
+++ app-modules/lookup-data/src/Providers/LookupDataServiceProvider.php
@@ -3,10 +3,14 @@
 namespace Modules\LookupData\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\LookupData\Public\LookupService;
 
 class LookupDataServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->singleton(LookupService::class);
+    }
 
     public function boot(): void {}
 }
